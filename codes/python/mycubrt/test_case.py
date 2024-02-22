9b071a7c180d61ba93f3c5db72d6bad15742d1c6
Ishmeet7 <ishmeetkaur645@gmail.com>
2024-02-22 11:15:19 +0530

    made changes

--- codes/python/mycubrt/test_case.py
+++ codes/python/mycubrt/test_case.py
@@ -1,20 +1,20 @@
 def test_1():
     from numpy import cbrt
     from calc_cubrt import my_cubrt
-    small=1.0e-10
+    small=1.0e-14
     xvalues=[27,1000,0.001,0,1e9]
     for x in xvalues:
-        s=mycubrt(x)
+        s=my_cubrt(x)
         s_numpy=cbrt(x)
         assert (s-s_numpy)<small ,f"cube root failed"
 
 def test_2():
     from numpy import cbrt
     from calc_cubrt import my_cubrt
-    small=1.0e-10
+    small=1.0e-14
     xvalues=[-27,-90,"10"]
     for x in xvalues:
-        s=mycubrt(x)
+        s=my_cubrt(x)
         s_numpy=cbrt(x)
         assert (s-s_numpy)<small ,f"cube root failed"
         
